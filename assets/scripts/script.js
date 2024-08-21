5b2ec46ae499896972e421360653a8e4199f6726
cgxvi <adam.ulfsbo@gmail.com>
2024-08-21 20:38:07 +0200

    Add files via upload

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -9,6 +9,6 @@ function updateURL() {
     const inputText = document.getElementById('userInput').value;
     const baseURL = 'https://doi-org.ezproxy.ub.gu.se/';
     const fullURL = baseURL + encodeURIComponent(inputText);
-    document.getElementById('result').innerText = fullURL;
+    const resultElement = document.getElementById('result');
+    resultElement.innerHTML = `<a href="${fullURL}" target="_blank">${fullURL}</a>`;
 }
-
