b193d8a2dcaafacfeb84511ea83ea80af244b8e6
cgxvi <adam.ulfsbo@gmail.com>
2024-09-02 09:23:55 +0200

    Add files via upload

--- assets/scripts/script_time.js
+++ assets/scripts/script_time.js
@@ -1,13 +1,23 @@
 function updateTime() {
     var now = new Date();
-    var formattedTime = now.getFullYear() + '-' +
+    
+    // Function to calculate the week number
+    function getWeekNumber(d) {
+        var oneJan = new Date(d.getFullYear(), 0, 1);
+        var numberOfDays = Math.floor((d - oneJan) / (24 * 60 * 60 * 1000));
+        return Math.ceil((d.getDay() + 1 + numberOfDays) / 7);
+    }
+
+    var weekNumber = getWeekNumber(now);
+    var formattedTime = 'Week ' + weekNumber + ' - ' +
+                        now.getFullYear() + '-' +
                         ('0' + (now.getMonth() + 1)).slice(-2) + '-' +
-                        ('0' + now.getDate()).slice(-2) + ' ' +
+                        ('0' + now.getDate()).slice(-2) + ' - ' +
                         ('0' + now.getHours()).slice(-2) + ':' +
                         ('0' + now.getMinutes()).slice(-2) + ':' +
                         ('0' + now.getSeconds()).slice(-2);
     document.getElementById('datetime').textContent = formattedTime;
-  }
+}
 
 setInterval(updateTime, 1000);
 updateTime(); // initial call to display time immediately
