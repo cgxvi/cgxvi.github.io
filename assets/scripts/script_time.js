1aecf70bdee95c3af639db8fef602678ce4391fb
cgxvi <adam.ulfsbo@gmail.com>
2024-09-06 07:59:23 +0200

    Add files via upload

--- assets/scripts/script_time.js
+++ assets/scripts/script_time.js
@@ -1,14 +1,20 @@
 function updateTime() {
     var now = new Date();
-    
-    // Function to calculate the week number
-    function getWeekNumber(d) {
-        var oneJan = new Date(d.getFullYear(), 0, 1);
-        var numberOfDays = Math.floor((d - oneJan) / (24 * 60 * 60 * 1000));
-        return Math.ceil((d.getDay() + 1 + numberOfDays) / 7);
+
+    // Function to calculate the ISO week number
+    function getISOWeekNumber(d) {
+        var target = new Date(d.valueOf());
+        var dayNr = (d.getDay() + 6) % 7;
+        target.setDate(target.getDate() - dayNr + 3);
+        var firstThursday = target.valueOf();
+        target.setMonth(0, 1);
+        if (target.getDay() !== 4) {
+            target.setMonth(0, 1 + ((4 - target.getDay()) + 7) % 7);
+        }
+        return 1 + Math.ceil((firstThursday - target) / (7 * 24 * 60 * 60 * 1000));
     }
 
-    var weekNumber = getWeekNumber(now);
+    var weekNumber = getISOWeekNumber(now);
     var formattedTime = 'Week ' + weekNumber + ' - ' +
                         now.getFullYear() + '-' +
                         ('0' + (now.getMonth() + 1)).slice(-2) + '-' +
